5c7387bcfca5101cd49c51c546922f9c95d45cb1
David Cebrian <davidcebriancortes0@gmail.com>
2023-04-13 13:12:48 +0200

    Lists and order exer

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -21,8 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  count = 0
+  for word in words:
+    if len(word) >= 2 and word.startswith(word[-1]):
+      count += 1
+  return count
 
 
 # B. front_x
@@ -33,8 +36,18 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
+  ordered_list_1 = []
+  ordered_list_2 = []
+  ordered_final_list = []
+  for word in words:
+    if word.startswith("x"):
+      ordered_list_1.append(word)
+    else:
+      ordered_list_2.append(word)
+  ordered_list_1.sort()
+  ordered_list_2.sort()
+  ordered_final_list = ordered_list_1 + ordered_list_2
+  return ordered_final_list
 
 
 
@@ -44,9 +57,11 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+def last_number(list):
+  return list[-1]
+
 def sort_last(tuples):
-  # +++your code here+++
-  return
+  return sorted (tuples, key=last_number)
 
 
 # Simple provided test() function used in main() to print
@@ -56,18 +71,18 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'match_ends'
+  print ('match_ends')
   test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
   test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
   test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
   print
-  print 'front_x'
+  print ('front_x')
   test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
        ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
   test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
@@ -77,7 +92,7 @@ def main():
 
        
   print
-  print 'sort_last'
+  print ('sort_last')
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
        [(2, 1), (3, 2), (1, 3)])
   test(sort_last([(2, 3), (1, 2), (3, 1)]),
--- google-python-exercises/basic/list2.py
+++ google-python-exercises/basic/list2.py
@@ -13,19 +13,25 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-  # +++your code here+++
-  return
-
+  for i in range(len(nums)-1, 0, -1):
+    if nums[i] == nums[i-1]:
+      nums.remove(nums[i])
+  return nums
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+  list_return = []
+  while list1 and list2:
+    if  list1[0] < list2[0]:
+      list_return.append(list1.pop(0))
+    else:
+      list_return.append(list2.pop(0))
+  return list_return + list1 + list2 
 
-# Note: the solution above is kind of cute, but unforunately list.pop(0)
+# Note: the solution above is kind of cute, but unfortunately list.pop(0)
 # is not constant time with the standard python list implementation, so
 # the above is not strictly linear time.
 # An alternate approach uses pop(-1) to remove the endmost elements
@@ -41,18 +47,18 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'remove_adjacent'
+  print ('remove_adjacent')
   test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
   test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
   test(remove_adjacent([]), [])
 
   print
-  print 'linear_merge'
+  print ('linear_merge')
   test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
        ['aa', 'bb', 'cc', 'xx', 'zz'])
   test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
